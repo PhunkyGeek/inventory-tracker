9a7d87cd0c45a3e5e73ea85d34ed5f9daf618d1d
Ronnie <ronaldabel1996@gmail.com>
2024-08-17 01:25:05 +0000

    Inventory Tracker codebase v1

--- firebase.js
+++ firebase.js
@@ -10,15 +10,16 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDfNAPlQ7x6BFS2Cs5Wh3zp4Kb0JDsNwz8",
-  authDomain: "inventory-tracker-6d9d7.firebaseapp.com",
-  projectId: "inventory-tracker-6d9d7",
-  storageBucket: "inventory-tracker-6d9d7.appspot.com",
-  messagingSenderId: "840218886293",
-  appId: "1:840218886293:web:179c53fb51c19ba9eadb9c",
-  measurementId: "G-8VY9KCLQJ9"
-};
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
